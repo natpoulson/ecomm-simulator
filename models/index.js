11ceb032183b4fce621054ec128acce56a46f960
Nat Poulson <nat.poulson@outlook.com.au>
2024-02-25 13:53:25 +1100

    MODEL: Define relationships

--- models/index.js
+++ models/index.js
@@ -5,12 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'product_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  foreignKey: 'tag_id',
+  through: 'product_tag'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tags.belongsToMany(Product, {
+  foreignKey: 'product_id',
+  through: 'product_tag'
+});
 
 module.exports = {
   Product,
